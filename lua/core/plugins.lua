25cfa7f976064e833283f4e22fbc26492b43a2df
Jana <jana@codi.coop>
2023-01-04 03:17:24 +0100

    Added markdown-preview plugin and some formatting changes

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -56,6 +56,10 @@ return packer.startup(function(use)
     use "goolord/alpha-nvim"
     use "lewis6991/impatient.nvim"
     use "lukas-reineke/indent-blankline.nvim"
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function() vim.fn["mkdp#util#install"]() end,
+    })
 
     -- Colorschemes
     use { "catppuccin/nvim", as = "catppuccin" }
@@ -66,23 +70,23 @@ return packer.startup(function(use)
         "VonHeikemen/lsp-zero.nvim",
         requires = {
             -- LSP Support
-            {"neovim/nvim-lspconfig"},
-            {"williamboman/mason.nvim"},
-            {"williamboman/mason-lspconfig.nvim"},
-            {"jose-elias-alvarez/null-ls.nvim"},
+            { "neovim/nvim-lspconfig" },
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
+            { "jose-elias-alvarez/null-ls.nvim" },
 
             -- Autocompletion
-            {"hrsh7th/nvim-cmp"},
-            {"hrsh7th/cmp-buffer"},
-            {"hrsh7th/cmp-path"},
-            { "hrsh7th/cmp-cmdline"},
-            {"saadparwaiz1/cmp_luasnip"},
-            {"hrsh7th/cmp-nvim-lsp"},
-            {"hrsh7th/cmp-nvim-lua"},
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "hrsh7th/cmp-cmdline" },
+            { "saadparwaiz1/cmp_luasnip" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-nvim-lua" },
 
             -- Snippets
-            {"L3MON4D3/LuaSnip"},
-            {"rafamadriz/friendly-snippets"},
+            { "L3MON4D3/LuaSnip" },
+            { "rafamadriz/friendly-snippets" },
         }
     }
 
